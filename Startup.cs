ecbf4f29b9d33148b680caebe484645c77d1a25c
Abhishikha Maurya <abhishikha.maurya1998@gmail.com>
2021-10-30 00:56:51 +0530

    [Abhishikha] refactor image path

--- Startup.cs
+++ Startup.cs
@@ -14,7 +14,6 @@ namespace testProject2
         {
             Configuration = configuration;
         }
-
         private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -24,8 +23,8 @@ namespace testProject2
             services.AddMvc(); // for sending the data in format of Json file.
             //services.AddMvc().AddXmlSerializerFormatters();
             
-            //services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>(); // IN-MEMORY COLLECTION
-            services.AddScoped<IEmployeeRepository, SqlEmployeeRepository>(); // Sequel-server database
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>(); // IN-MEMORY COLLECTION
+            //services.AddScoped<IEmployeeRepository, SqlEmployeeRepository>(); // Sequel-server database
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
